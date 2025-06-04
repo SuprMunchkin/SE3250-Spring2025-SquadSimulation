Fixed probability calculation. (#28)

models/squad_simulation.py
@@ -133,7 +133,11 @@ def run_simulation(params):
             blue_patrol['direction'] = 270
 
         distance = np.sqrt((blue_patrol['x'] - hostile_patrol['x'])**2 + (blue_patrol['y'] - hostile_patrol['y'])**2)
-        prob_attack = min(1, 100 / distance if distance > 0 else 1)
+        if distance != 0:
+            prob_attack = 1 / np.sqrt(distance)
+        else:
+            prob_attack = 1
+                    
         if distance <= 1000 and np.random.random() < prob_attack:
             blue_kills, hostile_kills = _attack(
                 blue_patrol, hostile_patrol, params['environment'],