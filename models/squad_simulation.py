bac3c71985e9fd37aee58df4e84ae245df8d71be
SuprMunchkin <justin.mathgeek.stephens00@gmail.com>
2025-06-04 22:28:57 +0000

    Pulled attack probability out of attack function and renamed some methods to use the convention for internal.

--- models/squad_simulation.py
+++ models/squad_simulation.py
@@ -23,13 +23,7 @@ def get_defeat_probability(armor, threat, velocity):
     exponent = beta0 + velocity * beta1
     return np.exp(exponent) / (1 + np.exp(exponent))
 
-def attack(blue_patrol, hostile_patrol, env, armor, fire_rates ,distance):
-    if distance > 1000:
-        return 0, 0
-    prob_attack = min(1, 100 / distance if distance > 0 else 1)
-    if np.random.random() > prob_attack:
-        return 0, 0
-
+def _attack(blue_patrol, hostile_patrol, env, armor, distance):
     # Blue shots
     blue_shots = np.random.randint(fire_rates["blue_min"], fire_rates["blue_max"] + 1) * blue_patrol['stock']
     prob_blue_hit = exp(-0.002 * distance)
@@ -139,10 +133,11 @@ def run_simulation(params):
             blue_patrol['direction'] = 270
 
         distance = np.sqrt((blue_patrol['x'] - hostile_patrol['x'])**2 + (blue_patrol['y'] - hostile_patrol['y'])**2)
-        if distance <= 1000:
-            blue_kills, hostile_kills = attack(
+        prob_attack = min(1, 100 / distance if distance > 0 else 1)
+        if distance <= 1000 and np.random.random() < prob_attack:
+            blue_kills, hostile_kills = _attack(
                 blue_patrol, hostile_patrol, params['environment'],
-                params['armor_type'], fire_rates, distance
+                params['armor_type'], distance
             )
             blue_patrol['stock'] -= blue_kills
             hostile_patrol['stock'] -= hostile_kills
